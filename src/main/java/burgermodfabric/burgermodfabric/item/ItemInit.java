084dc4063ea6b86a9fb70d379d6ac294df996c24
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2022-05-01 15:47:21 -0400

    Added Cheese & Champignon burgers

--- src/main/java/burgermodfabric/burgermodfabric/item/ItemInit.java
+++ src/main/java/burgermodfabric/burgermodfabric/item/ItemInit.java
@@ -13,6 +13,25 @@ public class ItemInit {
     public static final Item MUTTON_BURGER = registerItem("mutton_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_2)));
     public static final Item CHICKEN_BURGER = registerItem("chicken_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_3)));
 
+    public static final Item SALMON_BURGER = registerItem("salmon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_4)));
+    public static final Item COD_BURGER = registerItem("cod_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_5)));
+
+    public static final Item BEEF_CHEESE_BURGER = registerItem("beef_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_1)));
+    public static final Item PORK_CHEESE_BURGER = registerItem("pork_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_1)));
+    public static final Item MUTTON_CHEESE_BURGER = registerItem("mutton_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_2)));
+    public static final Item CHICKEN_CHEESE_BURGER = registerItem("chicken_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_3)));
+
+    public static final Item SALMON_CHEESE_BURGER = registerItem("salmon_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_4)));
+    public static final Item COD_CHEESE_BURGER = registerItem("cod_cheese_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_5)));
+
+    public static final Item BEEF_CHAMPIGNON_BURGER = registerItem("beef_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_1)));
+    public static final Item PORK_CHAMPIGNON_BURGER = registerItem("pork_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_1)));
+    public static final Item MUTTON_CHAMPIGNON_BURGER = registerItem("mutton_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_2)));
+    public static final Item CHICKEN_CHAMPIGNON_BURGER = registerItem("chicken_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_3)));
+
+    public static final Item SALMON_CHAMPIGNON_BURGER = registerItem("salmon_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_4)));
+    public static final Item COD_CHAMPIGNON_BURGER = registerItem("cod_champignon_burger", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.BURGER_TIER_5)));
+
     public static final Item SCRAMBLED_EGG = registerItem("scrambled_egg", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.RAW_EGG_FOOD)));
     public static final Item FRIED_SCRAMBLED_EGG = registerItem("fried_scrambled_egg", new Item(new FabricItemSettings().group(ModItemGroup.BURGERMOD_TAB).food(ModFoodComponets.FRIED_EGG_FOOD)));
 
--- src/main/java/burgermodfabric/burgermodfabric/item/ModFoodComponets.java
+++ src/main/java/burgermodfabric/burgermodfabric/item/ModFoodComponets.java
@@ -4,12 +4,15 @@ import burgermodfabric.burgermodfabric.effect.ModEffects;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
+import org.apache.commons.lang3.function.FailableObjDoubleConsumer;
 
 public class ModFoodComponets {
 
     public static final FoodComponent BURGER_TIER_1 = new FoodComponent.Builder().hunger(10).meat().saturationModifier(1.0f).build();
     public static final FoodComponent BURGER_TIER_2 = new FoodComponent.Builder().hunger(8).meat().saturationModifier(1.0f).build();
     public static final FoodComponent BURGER_TIER_3 = new FoodComponent.Builder().hunger(8).meat().saturationModifier(0.8f).build();
+    public static final FoodComponent BURGER_TIER_4 = new FoodComponent.Builder().hunger(8).saturationModifier(1.0f).build();
+    public static final FoodComponent BURGER_TIER_5 = new FoodComponent.Builder().hunger(7).saturationModifier(0.8f).build();
 
     public static final FoodComponent RAW_EGG_FOOD = new FoodComponent.Builder().hunger(1).saturationModifier(0.1f).statusEffect(new StatusEffectInstance(ModEffects.RAW_SCRAMBLED_EGG_EFFECTS, 200, 0), 1.0f).build();
     public static final FoodComponent FRIED_EGG_FOOD = new FoodComponent.Builder().hunger(2).meat().saturationModifier(0.6f).build();
